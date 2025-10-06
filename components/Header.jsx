aff8dfe68972cd8b41c54ecbfe37311a69197879
NomadNiko <nomad.niko.inc@gmail.com>
2025-10-05 19:12:36 -1000

    feat: fixing buttons

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,42 +1,58 @@
-'use client'
-import { useTranslations } from 'next-intl';
+"use client";
+import { useTranslations } from "next-intl";
 
 export default function Header() {
-    const t = useTranslations('header');
+  const t = useTranslations("header");
 
-    return (
-        <div className="w-11/12 max-w-3xl text-center mx-auto min-h-screen flex flex-col items-center justify-center gap-4 py-32 md:py-0">
-            <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo">
-                {t('welcome')}
-                <img src="/assets/hand-icon.png" alt="" className="w-6 mb-1" />
-            </h3>
-            <div className="relative group">
-                <div className="absolute inset-0 -m-2 animate-rotate_gradient rounded-full">
-                    <div className="w-full h-full bg-gradient-to-r from-fuchsia-600 via-amber-600 to-green-600 opacity-60 blur-lg rounded-full"></div>
-                </div>
-                <img src="/assets/logo_heart.png" alt="" className="w-48 relative group-hover:scale-110 transition-transform duration-500" />
-            </div>
-            <h1 className="text-5xl sm:text-6xl font-Ovo">{t('tagline')}</h1>
-            <p className="max-w-2xl mx-auto font-Ovo text-xl md:text-2xl">
-                {t('description.line1')}<br />
-                {t('description.line2')}<br />
-                {t('description.line3')}
-            </p>
-
-            <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
-                <div className="rainbow-button relative z-0 overflow-hidden p-0.5 rounded-full hover:scale-105 transition duration-300 active:scale-100">
-                    <div className="rainbow-before absolute left-[-50%] top-[-50%] w-[200%] h-[200%] animate-rotate_border -z-10 bg-gradient-to-r from-[#b820e6] to-[#da7d20] blur-md"></div>
-                    <a href="#contact"
-                        className="px-10 py-2.5 rounded-full bg-gradient-to-r from-[#b820e6] to-[#da7d20] text-white flex items-center gap-2 relative z-10">
-                        {t('buttons.joinWaitlist')} <img src="/assets/right-arrow-white.png" alt="" className="w-4" />
-                    </a>
-                </div>
-
-                <a href="#about"
-                    className="px-10 py-2.5 rounded-full border border-gray-300 dark:border-white/25 hover:bg-slate-100/70 dark:hover:bg-darkHover flex items-center gap-2 bg-white dark:bg-transparent dark:text-white">
-                    {t('buttons.learnMore')} <img src="/assets/download-icon.png" alt="" className="w-4 dark:invert" />
-                </a>
-            </div>
+  return (
+    <div className="flex flex-col items-center justify-center w-11/12 max-w-3xl min-h-screen gap-4 py-32 mx-auto text-center md:py-0">
+      <h3 className="flex items-end gap-2 mb-3 text-xl md:text-2xl font-Ovo">
+        {t("welcome")}
+        <img src="/assets/hand-icon.png" alt="" className="w-6 mb-1" />
+      </h3>
+      <div className="relative group">
+        <div className="absolute inset-0 -m-2 rounded-full animate-rotate_gradient">
+          <div className="w-full h-full rounded-full bg-gradient-to-r from-fuchsia-600 via-amber-600 to-green-600 opacity-60 blur-lg"></div>
         </div>
-    )
+        <img
+          src="/assets/logo_heart.png"
+          alt=""
+          className="relative w-48 transition-transform duration-500 group-hover:scale-110"
+        />
+      </div>
+      <h1 className="text-5xl sm:text-6xl font-Ovo">{t("tagline")}</h1>
+      <p className="max-w-2xl mx-auto text-xl font-Ovo md:text-2xl">
+        {t("description.line1")}
+        <br />
+        {t("description.line2")}
+        <br />
+        {t("description.line3")}
+      </p>
+
+      <div className="flex flex-col items-center gap-4 mt-4 sm:flex-row">
+        <div className="rainbow-button relative z-0 overflow-hidden p-0.5 rounded-full hover:scale-105 transition duration-300 active:scale-100">
+          <div className="rainbow-before absolute left-[-50%] top-[-50%] w-[200%] h-[200%] animate-rotate_border -z-10 bg-gradient-to-r from-[#202de6] to-[#da2020]  blur-md"></div>
+          <a
+            href="#contact"
+            className="px-10 py-2.5 rounded-full bg-gradient-to-r from-[#202de6] to-[#da2020]  text-white flex items-center gap-2 relative z-10"
+          >
+            {t("buttons.joinWaitlist")}{" "}
+            <img src="/assets/right-arrow-white.png" alt="" className="w-4" />
+          </a>
+        </div>
+
+        <a
+          href="#about"
+          className="px-10 py-2.5 rounded-full border border-gray-300 dark:border-white/25 hover:bg-slate-100/70 dark:hover:bg-darkHover flex items-center gap-2 bg-white dark:bg-transparent dark:text-white"
+        >
+          {t("buttons.learnMore")}{" "}
+          <img
+            src="/assets/download-icon.png"
+            alt=""
+            className="w-4 dark:invert"
+          />
+        </a>
+      </div>
+    </div>
+  );
 }
--- components/Services.jsx
+++ components/Services.jsx
@@ -1,61 +1,77 @@
-'use client'
-import { useTranslations } from 'next-intl';
+"use client";
+import { useTranslations } from "next-intl";
 
 export default function Services() {
-    const t = useTranslations('services');
+  const t = useTranslations("services");
 
-    const services = [
-        {
-            name: t('features.idVerification.title'),
-            icon: '/assets/web-icon.png',
-            description: t('features.idVerification.description'),
-            link: '',
-        },
-        {
-            name: t('features.conversationFirst.title'),
-            icon: '/assets/mobile-icon.png',
-            description: t('features.conversationFirst.description'),
-            link: '',
-        },
-        {
-            name: t('features.mutualReveal.title'),
-            icon: '/assets/ui-icon.png',
-            description: t('features.mutualReveal.description'),
-            link: '',
-        },
-        {
-            name: t('features.safeAndSecure.title'),
-            icon: '/assets/graphics-icon.png',
-            description: t('features.safeAndSecure.description'),
-            link: '',
-        }
-    ];
-    return (
-        <div id="services" className="w-full px-[12%] py-10 scroll-mt-20">
-            <h4 className="text-center mb-2 text-xl md:text-2xl font-Ovo">{t('sectionTitle')}</h4>
-            <h2 className="text-center text-5xl sm:text-6xl font-Ovo">{t('mainTitle')}</h2>
-            <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo text-base md:text-lg">{t('description')}</p>
+  const services = [
+    {
+      name: t("features.idVerification.title"),
+      icon: "/assets/web-icon.png",
+      description: t("features.idVerification.description"),
+      link: "",
+    },
+    {
+      name: t("features.conversationFirst.title"),
+      icon: "/assets/mobile-icon.png",
+      description: t("features.conversationFirst.description"),
+      link: "",
+    },
+    {
+      name: t("features.mutualReveal.title"),
+      icon: "/assets/ui-icon.png",
+      description: t("features.mutualReveal.description"),
+      link: "",
+    },
+    {
+      name: t("features.safeAndSecure.title"),
+      icon: "/assets/graphics-icon.png",
+      description: t("features.safeAndSecure.description"),
+      link: "",
+    },
+  ];
+  return (
+    <div id="services" className="w-full px-[12%] py-10 scroll-mt-20">
+      <h4 className="mb-2 text-xl text-center md:text-2xl font-Ovo">
+        {t("sectionTitle")}
+      </h4>
+      <h2 className="text-5xl text-center sm:text-6xl font-Ovo">
+        {t("mainTitle")}
+      </h2>
+      <p className="max-w-2xl mx-auto mt-5 mb-12 text-base text-center font-Ovo md:text-lg">
+        {t("description")}
+      </p>
 
-            <div className="grid grid-cols-auto gap-6 my-10">
-                {services.map((service) => (
-                    <div key={service.name} className="border border-gray-300 dark:border-white/30 rounded-lg px-8 py-12 hover:shadow-black hover:bg-lightHover hover:-translate-y-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow-white">
-                        <img src={service.icon} alt="" className="w-10" />
-                        <h3 className="my-4 text-gray-700 dark:text-white text-xl md:text-2xl">{service.name}</h3>
-                        <p className="text-gray-600 leading-5 dark:text-white/80 text-base md:text-lg">{service.description}</p>
-                    </div>
-                ))}
-            </div>
+      <div className="grid gap-6 my-10 grid-cols-auto">
+        {services.map((service) => (
+          <div
+            key={service.name}
+            className="px-8 py-12 duration-500 border border-gray-300 rounded-lg dark:border-white/30 hover:shadow-black hover:bg-lightHover hover:-translate-y-1 dark:hover:bg-darkHover dark:hover:shadow-white"
+          >
+            <img src={service.icon} alt="" className="w-10" />
+            <h3 className="my-4 text-xl text-gray-700 dark:text-white md:text-2xl">
+              {service.name}
+            </h3>
+            <p className="text-base leading-5 text-gray-600 dark:text-white/80 md:text-lg">
+              {service.description}
+            </p>
+          </div>
+        ))}
+      </div>
 
-            {/* Join Waitlist Button */}
-            <div className="flex justify-center my-20">
-                <div className="rainbow-button relative z-0 overflow-hidden p-0.5 rounded-full hover:scale-105 transition duration-300 active:scale-100">
-                    <div className="rainbow-before absolute left-[-50%] top-[-50%] w-[200%] h-[200%] animate-rotate_border -z-10 bg-gradient-to-r from-[#b820e6] to-[#da7d20] blur-md"></div>
-                    <a href="#contact"
-                        className="px-10 py-2.5 rounded-full bg-gradient-to-r from-[#b820e6] to-[#da7d20] text-white flex items-center gap-2 relative z-10">
-                        {t('buttons.joinWaitlist')} <img src="/assets/right-arrow-white.png" alt="" className="w-4" />
-                    </a>
-                </div>
-            </div>
+      {/* Join Waitlist Button */}
+      <div className="flex justify-center my-20">
+        <div className="rainbow-button relative z-0 overflow-hidden p-0.5 rounded-full hover:scale-105 transition duration-300 active:scale-100">
+          <div className="rainbow-before absolute left-[-50%] top-[-50%] w-[200%] h-[200%] animate-rotate_border -z-10 bg-gradient-to-r from-[#202de6] to-[#da2020] blur-md"></div>
+          <a
+            href="#contact"
+            className="px-10 py-2.5 rounded-full bg-gradient-to-r from-[#202de6] to-[#da2020]  text-white flex items-center gap-2 relative z-10"
+          >
+            {t("buttons.joinWaitlist")}{" "}
+            <img src="/assets/right-arrow-white.png" alt="" className="w-4" />
+          </a>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
